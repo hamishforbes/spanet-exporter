950c7b48b652622669d8cd321be243996f8f0200
Hamish <hamish.forbes@gmail.com>
2022-03-01 15:20:01 +1300

    Improve RF parsing, debug loggin

--- spanet_client/spa_client.go
+++ spanet_client/spa_client.go
@@ -11,7 +11,7 @@ import (
 type SpaPumpAttributes struct {
 	Id        int
 	Active    float64
-	Installed float64
+	Installed string
 	Ok        float64
 }
 
@@ -98,30 +98,89 @@ func (s *SpaConn) Connect() error {
 	return nil
 }
 
-func getFloatAttribute(data [][]string, row int, col int) float64 {
-	if len(data) < row {
-		return 0
+func getStringAttribute(data map[string][]string, row string, col int) string {
+	if val, ok := data[row]; ok {
+		if len(val) >= col {
+			return val[col]
+		} else {
+			fmt.Println(fmt.Sprintf("Line %s has no col %d, len %d", row, col, len(val)))
+		}
+	} else {
+		fmt.Println("Data has no row: " + row)
 	}
-	if len(data[row]) < col {
-		return 0
-	}
-	v, _ := strconv.ParseFloat(data[row][col], 64)
+
+	return ""
+}
+
+func getFloatAttribute(data map[string][]string, row string, col int) float64 {
+	v, _ := strconv.ParseFloat(getStringAttribute(data, row, col), 64)
 	return v
 }
 
-func getBoolAttribute(data [][]string, row int, col int) float64 {
-	if len(data) < row {
-		return 0
-	}
-	if len(data[row]) < col {
-		return 0
-	}
-	v, _ := strconv.ParseBool(data[row][col])
+func getBoolAttribute(data map[string][]string, row string, col int) float64 {
+	v, _ := strconv.ParseBool(getStringAttribute(data, row, col))
 	if v {
 		return 1
 	} else {
 		return 0
 	}
+}
+
+func parsePumpInstalled(v string) string {
+	/*
+		`1-1-014`
+		First part (1- or 0-) indicates whether the pump is installed/fitted. If so (1- means it is)
+		the second part indicates it's speed type.
+		The third part represents it's possible states (0 OFF, 1 ON, 4 AUTO)
+	*/
+	return v
+}
+
+func parseRfResponse(data string) map[string][]string {
+	/*
+		RF:
+		,R2,18,250,51,70,4,13,50,55,19,6,2020,376,9999,1,0,490,207,34,6000,602,23,20,0,0,0,0,44,35,45,:
+		,R3,32,1,4,4,4,SW V5 17 05 31,SV3,18480001,20000826,1,0,0,0,0,0,NA,7,0,470,Filtering,4,0,7,7,0,0,:
+		,R4,NORM,0,0,0,1,0,3547,4,20,4500,7413,567,1686,0,8388608,0,0,5,0,98,0,10084,4,80,100,0,0,4,:
+		,R5,0,1,0,1,0,0,0,0,0,0,1,0,1,0,376,0,3,4,0,0,0,0,0,1,2,6,:
+		,R6,1,5,30,2,5,8,1,360,1,0,3584,5120,127,128,5632,5632,2304,1792,0,30,0,0,0,0,2,3,0,:
+		,R7,2304,0,1,1,1,0,1,0,0,0,253,191,253,240,483,125,77,1,0,0,0,23,200,1,0,1,31,32,35,100,5,:
+		,R9,F1,255,0,0,0,0,0,0,0,0,0,0,:
+		,RA,F2,0,0,0,0,0,0,255,0,0,0,0,:
+		,RB,F3,0,0,0,0,0,0,0,0,0,0,0,:
+		,RC,0,1,1,0,0,0,0,0,0,2,0,0,1,0,:
+		,RE,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,-4,13,30,8,5,1,0,0,0,0,0,:*
+		,RG,1,1,1,1,1,1,1-1-014,1-1-01,1-1-01,0-,0-,0,:*
+	*/
+	rows := strings.Split(data, "\n")
+	ret := map[string][]string{}
+	lines := []string{
+		"R2",
+		"R3",
+		"R4",
+		"R5",
+		"R6",
+		"R7",
+		// No 8...
+		"R9",
+		"RA",
+		"RB",
+		"RC",
+		// No D...
+		"RE",
+		"RG",
+	}
+
+	for _, rp := range lines {
+		for _, row := range rows {
+			if strings.HasPrefix(row, ","+rp) {
+				ret[rp] = strings.Split(row[4:len(row)-2], ",")
+				//fmt.Println(fmt.Sprintf("Row  %s: %s", rp, row))
+			}
+		}
+	}
+
+	return ret
 }
 
 func (s *SpaConn) Read() (SpaAttributes, error) {
@@ -139,80 +198,64 @@ func (s *SpaConn) Read() (SpaAttributes, error) {
 		return attributes, errors.New("Malformed Response")
 	}
 
-	//fmt.Println(reply)
-	/*
-		RF:
-		,R2,18,250,51,70,4,13,50,55,19,6,2020,376,9999,1,0,490,207,34,6000,602,23,20,0,0,0,0,44,35,45,:
-		,R3,32,1,4,4,4,SW V5 17 05 31,SV3,18480001,20000826,1,0,0,0,0,0,NA,7,0,470,Filtering,4,0,7,7,0,0,:
-		,R4,NORM,0,0,0,1,0,3547,4,20,4500,7413,567,1686,0,8388608,0,0,5,0,98,0,10084,4,80,100,0,0,4,:
-		,R5,0,1,0,1,0,0,0,0,0,0,1,0,1,0,376,0,3,4,0,0,0,0,0,1,2,6,:
-		,R6,1,5,30,2,5,8,1,360,1,0,3584,5120,127,128,5632,5632,2304,1792,0,30,0,0,0,0,2,3,0,:
-		,R7,2304,0,1,1,1,0,1,0,0,0,253,191,253,240,483,125,77,1,0,0,0,23,200,1,0,1,31,32,35,100,5,:
-		,R9,F1,255,0,0,0,0,0,0,0,0,0,0,:
-		,RA,F2,0,0,0,0,0,0,255,0,0,0,0,:
-		,RB,F3,0,0,0,0,0,0,0,0,0,0,0,:
-		,RC,0,1,1,0,0,0,0,0,0,2,0,0,1,0,:
-		,RE,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,-4,13,30,8,5,1,0,0,0,0,0,:*
-		,RG,1,1,1,1,1,1,1-1-014,1-1-01,1-1-01,0-,0-,0,:*
-	*/
+	fmt.Println(reply)
+	fmt.Println("-----")
 
-	data := [][]string{}
-	for i, r := range strings.Split(reply, "\n") {
-		if i == 0 {
-			continue
-		}
-		//fmt.Println(fmt.Sprintf("Row %d: %s", i-1, r[4:len(r)-2]))
-		var row = []string{}
-		for _, v := range strings.Split(r[4:len(r)-2], ",") {
-			row = append(row, v)
-		}
-		data = append(data, row)
-	}
+	data := parseRfResponse(reply)
 
-	attributes.WaterTemperature = getFloatAttribute(data, 3, 14) / 10
-	attributes.TargetTemperature = getFloatAttribute(data, 4, 7) / 10
-	attributes.Heating = getBoolAttribute(data, 3, 11)
-	attributes.Auto = getBoolAttribute(data, 3, 11)
-	attributes.Sanitising = getBoolAttribute(data, 3, 15)
-	attributes.Cleaning = getBoolAttribute(data, 3, 10)
-	attributes.Sleeping = getBoolAttribute(data, 3, 9)
+	fmt.Println(fmt.Sprintf("R5: %s", strings.Join(data["R5"], ",")))
+	fmt.Println(getFloatAttribute(data, "R5", 14))
 
-	attributes.Blower.Mode = getFloatAttribute(data, 10, 9)
-	attributes.Blower.Speed = getFloatAttribute(data, 4, 0)
+	attributes.WaterTemperature = getFloatAttribute(data, "R5", 14) / 10
+	attributes.TargetTemperature = getFloatAttribute(data, "R6", 7) / 10
+	attributes.Heating = getBoolAttribute(data, "R5", 11)
+	attributes.Auto = getBoolAttribute(data, "R5", 11)
+	attributes.Sanitising = getBoolAttribute(data, "R5", 15)
+	attributes.Cleaning = getBoolAttribute(data, "R5", 10)
+	attributes.Sleeping = getBoolAttribute(data, "R5", 9)
 
-	attributes.Lights.Active = getBoolAttribute(data, 3, 13)
-	attributes.Lights.Mode = getFloatAttribute(data, 4, 3)
-	attributes.Lights.Brightness = getFloatAttribute(data, 4, 1)
-	attributes.Lights.Speed = getFloatAttribute(data, 4, 4)
-	attributes.Lights.Colour = getFloatAttribute(data, 4, 1)
+	attributes.Blower.Mode = getFloatAttribute(data, "RC", 9)
+	attributes.Blower.Speed = getFloatAttribute(data, "R6", 0)
+
+	attributes.Lights.Active = getBoolAttribute(data, "R5", 13)
+	attributes.Lights.Mode = getFloatAttribute(data, "R6", 3)
+	attributes.Lights.Brightness = getFloatAttribute(data, "R6", 1)
+	attributes.Lights.Speed = getFloatAttribute(data, "R6", 4)
+	attributes.Lights.Colour = getFloatAttribute(data, "R6", 1)
 
 	attributes.Pumps = [5]SpaPumpAttributes{}
 	attributes.Pumps[0].Id = 1
-	attributes.Pumps[0].Installed = getBoolAttribute(data, 11, 6)
-	attributes.Pumps[0].Active = getBoolAttribute(data, 3, 17)
+	attributes.Pumps[0].Installed = getStringAttribute(data, "RG", 6)
+	attributes.Pumps[0].Active = getBoolAttribute(data, "R5", 17)
 
 	attributes.Pumps[1].Id = 2
-	attributes.Pumps[1].Installed = getBoolAttribute(data, 11, 7)
-	attributes.Pumps[1].Active = getBoolAttribute(data, 3, 19)
-	attributes.Pumps[1].Ok = getBoolAttribute(data, 11, 1)
+	attributes.Pumps[1].Installed = getStringAttribute(data, "RG", 7)
+	attributes.Pumps[1].Active = getBoolAttribute(data, "R5", 19)
+	attributes.Pumps[1].Ok = getBoolAttribute(data, "RG", 1)
 
 	attributes.Pumps[2].Id = 3
-	attributes.Pumps[2].Installed = getBoolAttribute(data, 11, 8)
-	attributes.Pumps[2].Active = getBoolAttribute(data, 3, 19)
-	attributes.Pumps[2].Ok = getBoolAttribute(data, 11, 2)
+	attributes.Pumps[2].Installed = getStringAttribute(data, "RG", 8)
+	attributes.Pumps[2].Active = getBoolAttribute(data, "R5", 19)
+	attributes.Pumps[2].Ok = getBoolAttribute(data, "RG", 2)
 
 	attributes.Pumps[3].Id = 4
-	attributes.Pumps[3].Installed = getBoolAttribute(data, 11, 9)
-	attributes.Pumps[3].Active = getBoolAttribute(data, 3, 20)
-	attributes.Pumps[3].Ok = getBoolAttribute(data, 11, 3)
+	attributes.Pumps[3].Installed = getStringAttribute(data, "RG", 9)
+	attributes.Pumps[3].Active = getBoolAttribute(data, "R5", 20)
+	attributes.Pumps[3].Ok = getBoolAttribute(data, "RG", 3)
 
 	attributes.Pumps[4].Id = 5
-	attributes.Pumps[4].Installed = getBoolAttribute(data, 11, 10)
-	attributes.Pumps[4].Active = getBoolAttribute(data, 3, 21)
-	attributes.Pumps[4].Ok = getBoolAttribute(data, 11, 4)
+	attributes.Pumps[4].Installed = getStringAttribute(data, "RG", 10)
+	attributes.Pumps[4].Active = getBoolAttribute(data, "R5", 21)
+	attributes.Pumps[4].Ok = getBoolAttribute(data, "RG", 4)
+
+	fmt.Printf("Pump 1 Installed: %s \n", attributes.Pumps[0].Installed)
+	fmt.Printf("Pump 2 Installed: %s \n", attributes.Pumps[1].Installed)
+	fmt.Printf("Pump 3 Installed: %s \n", attributes.Pumps[2].Installed)
+	fmt.Printf("Pump 4 Installed: %s \n", attributes.Pumps[3].Installed)
+	fmt.Printf("Pump 5 Installed: %s \n", attributes.Pumps[4].Installed)
 
 	attributes.Settings = SpaSettings{
-		Lock: getFloatAttribute(data, 11, 11),
+		Lock: getFloatAttribute(data, "RG", 11),
 	}
 
 	return attributes, nil
